bc4a6898b4a4a4dd1fab26e5c356490dd4510c06
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-03 14:59:35 +0100

    document.write_Lev1_1

---
+++ assets/Js/main.js
@@ -0,0 +1,13 @@
+
+document.write("<h1>Hello World</h1>")
+document.write("<p> Have a nice day</p>")
+
+let myText = "Hello again<br><br>"
+document.write (myText)
+
+let vorname = "Markus<br>"
+let nachname = "Schneider"
+
+document.write("Name: " + vorname)
+document.write("Nachname: " + nachname)
+
